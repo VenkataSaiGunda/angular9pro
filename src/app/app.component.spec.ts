a8e19ec201d40748b2f1e7b603bdc9eb87012bd9
venkata Sai Gunda <gundavenkatasai@gmail.com>
2022-04-19 11:16:39 -0400

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,73 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular9pro'`, () => {
+  it(`should have as title 'Date'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular9pro');
+    expect(app.title).toEqual('Date');
   });
 
-  it('should render title', () => {
+  it(`should have as text 'Weather and time'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.text).toEqual('Weather and time');
+  });
+
+  it(`should have as update ''Every second'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.update).toEqual('Every second');
+  });
+
+  it(`should have as Weather 'OpenWeatherMap'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.Weather).toEqual('OpenWeatherMap');
+  });
+
+  it(`should have as API_KEY 'd586997381dc1b5e675e594118f5f003'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.API_KEY).toEqual('d586997381dc1b5e675e594118f5f003');
+  });
+
+  it(`should have as Icons 'fontawesome'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.Icons).toEqual('fontawesome');
+  });
+
+  it(`should have as city 'Toronto'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.city).toEqual('Toronto');
+  });
+
+  it(`should have as Test 'Unit Testing'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.Test).toEqual('Unit Testing');
+  });
+
+  it(`should have as CI 'Travis'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.CI).toEqual('Travis');
+  });
+
+
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular9pro app is running!');
-  });
+    const compiled = fixture.debugElement.nativeElement;
+  expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular project');
+ });
+
+ it('should render title in a h4 tag', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.debugElement.nativeElement;
+expect(compiled.querySelector('h4').textContent).toContain('This website provides weather updates');
+});
+
 });
